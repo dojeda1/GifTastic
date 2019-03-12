5a2aaf231697f9462778023a7c1db26bad8628d3
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-03-12 17:26:11 -0600

    Clicking any gifBtn adds a gif.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -30,4 +30,29 @@ $("#addButton").on("click", function () {
 
     placeButtons();
 
-})
+});
+
+function displayGifs() {
+
+    var getTerm = $(this).attr("data-term");
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + getTerm;
+
+    $.ajax({
+
+            url: queryURL,
+            method: "GET"
+        })
+
+        .then(function (response) {
+
+            var imageUrl = response.data.image_original_url;
+            var newImage = $("<img>");
+            newImage.attr("src", imageUrl);
+            newImage.attr("alt", "Gif");
+
+            $("#gifImages").prepend(newImage);
+
+        })
+}
+
+$(".gifBtn").on("click", displayGifs)
